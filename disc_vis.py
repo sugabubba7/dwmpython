baa0fdfa2bf95663b5fea54fada78849c02159a0
Pratik Mulik <120821308+pm0049@users.noreply.github.com>
2023-11-02 23:06:47 +0530

    Update disc_vis.py

--- disc_vis.py
+++ disc_vis.py
@@ -1,3 +1,4 @@
+#1
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -27,4 +28,27 @@ plt.xlabel('Value')
 plt.ylabel('Bin')
 plt.title('Scatter Plot of Value vs. Bin')
 plt.yticks(df['bin'].unique())
+plt.show()
+
+#2
+#2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# creating the dataset
+data = {'C':20, 'C++':15, 'Java':30,
+        'Python':35}
+courses = list(data.keys())
+values = list(data.values())
+
+fig = plt.figure(figsize = (10, 5))
+
+# creating the bar plot
+plt.bar(courses, values, color ='maroon',
+        width = 0.4)
+
+plt.xlabel("Courses offered")
+plt.ylabel("No. of students enrolled")
+plt.title("Students enrolled in different courses")
 plt.show()
