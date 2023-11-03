78e35b06695d1365acbdbbc9632f3e416bf548b3
Sumit Singh <109894162+sugabubba7@users.noreply.github.com>
2023-11-03 13:23:32 +0530

    Update K_means.py

--- K_means.py
+++ K_means.py
@@ -1,26 +1,100 @@
-# Importing necessary libraries
-import numpy as np
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-
-# Generating random data points for demonstration
-np.random.seed(0)
-X = np.random.rand(100, 2)  # 100 data points with 2 features each
-
-# Initializing KMeans with the number of clusters you want to form
-kmeans = KMeans(n_clusters=3)
-
-# Fitting the data to the KMeans algorithm
-kmeans.fit(X)
-
-# Getting the centroids and labels based on the trained KMeans algorithm
-centroids = kmeans.cluster_centers_
-labels = kmeans.labels_
-
-# Visualizing the data points and centroids
-plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
-plt.scatter(centroids[:, 0], centroids[:, 1], marker='X', s=200, linewidths=3, color='r')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('KMeans Clustering')
-plt.show()
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int i1,i2,i3,t1,t2;
+ 
+    int k0[10];
+    int k1[10];
+    int k2[10];
+ 
+    cout<<"\nEnter 10 numbers:\n";
+    for(i1=0;i1<10;i1++)
+    {
+        cin>>k0[i1];
+    }
+ 
+ 
+    //initial means
+    int m1;
+    int m2;
+ 
+    cout<<"\n Enter initial mean 1:";
+    cin>>m1;
+    cout<<"\n Enter initial mean 2:";
+    cin>>m2;
+     
+    int om1,om2;    //old means
+ 
+    do
+    {
+     
+    //saving old means
+    om1=m1;
+    om2=m2;
+ 
+    //creating clusters
+    i1=i2=i3=0;
+    for(i1=0;i1<10;i1++)
+    {
+        //calculating distance to means
+        t1=k0[i1]-m1;
+        if(t1<0){t1=-t1;}
+ 
+        t2=k0[i1]-m2;
+        if(t2<0){t2=-t2;}
+ 
+        if(t1<t2)
+        {
+            //near to first mean
+            k1[i2]=k0[i1];
+            i2++;
+        }
+        else
+        {
+            //near to second mean
+            k2[i3]=k0[i1];
+            i3++;
+        }
+ 
+    }
+ 
+    t2=0;
+    //calculating new mean
+    for(t1=0;t1<i2;t1++)
+    {
+        t2=t2+k1[t1];
+    }
+    m1=t2/i2;
+ 
+    t2=0;
+    for(t1=0;t1<i3;t1++)
+    {
+        t2=t2+k2[t1];
+    }
+    m2=t2/i3;
+ 
+    //printing clusters
+    cout<<"\nCluster 1:";
+    for(t1=0;t1<i2;t1++)
+    {
+        cout<<k1[t1]<<" ";
+    }
+    cout<<"\nm1="<<m1;
+ 
+    cout<<"\nCluster 2:";
+    for(t1=0;t1<i3;t1++)
+    {
+        cout<<k2[t1]<<" ";
+    }
+    cout<<"\nm2="<<m2;
+ 
+    cout<<"\n ----";
+    }while(m1!=om1&&m2!=om2);
+ 
+    cout<<"\n Clusters created";
+ 
+    //ending
+    getch();
+}
+ 
